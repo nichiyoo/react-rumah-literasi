538690847594488daf4c7201dde9c56ac3a9c3c1
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2025-03-27 16:23:57 +0700

    feat: refactor book-grid component to use custom result state hook, improve error handling and loading states

--- frontend/src/components/books/book-grid.jsx
+++ frontend/src/components/books/book-grid.jsx
@@ -3,31 +3,17 @@ import * as React from 'react';
 import { cn } from '@/libs/utils';
 
 import { useTransactionStore } from '@/store/use-transactions';
+import { useResultState } from '@/hooks/use-result-state';
 
+import { BookCard } from '@/components/books/book-card';
 import { Loading } from '@/components/loading';
 import { Empty } from '@/components/empty';
-import { BookCard } from '@/components/books/book-card';
+import { Error } from '@/components/error';
 
 const BookGrid = ({ className }) => {
 	const { append } = useTransactionStore();
-
-	const {
-		error,
-		data: result = { data: [] },
-		isLoading: loading,
-	} = useSWR('/books');
-
-	const empty = !error && !loading && result.data.length == 0;
-
-	const duplicate = React.useMemo(() => {
-		return {
-			data: Array.from({ length: 20 }, (_, i) => ({
-				...result.data[i % result.data.length],
-				id: i,
-				amount: 1,
-			})),
-		};
-	}, [result]);
+	const { error, data, isLoading: loading } = useSWR('/books');
+	const { result, empty } = useResultState(error, loading, data);
 
 	return (
 		<div
@@ -35,14 +21,11 @@ const BookGrid = ({ className }) => {
 				'grid grid-cols-2 gap-6 lg:grid-cols-3 xl:grid-cols-4',
 				className
 			)}>
-			{error ? (
-				<Error error={error} loading={loading} />
-			) : (
-				duplicate.data.map((book) => (
-					<BookCard book={book} key={book.id} onClick={() => append(book)} />
-				))
-			)}
+			{result.map((book) => (
+				<BookCard book={book} key={book.id} onClick={() => append(book)} />
+			))}
 
+			<Error error={!loading && error} />
 			<Empty empty={!loading && empty} />
 			<Loading loading={loading} />
 		</div>
